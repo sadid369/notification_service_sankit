de36772d9a4ac622a27e8f3e584740718fc3fa39
Sadid <66120003+sadid369@users.noreply.github.com>
2024-10-08 19:40:57 +0600

    created nodemailer transport object and tested

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-
+const mailSender = require('./config/email-config');
 const app = express();
+
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try {
+        const response = await mailSender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: "sadiddev369@gmail.com",
+            subject: 'Test',
+            text: 'test'
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 });
